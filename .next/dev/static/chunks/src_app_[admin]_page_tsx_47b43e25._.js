97571f25e70a24250d635f220ebe409efab39b8e
Dhanushka Ashanka Bandara <dhanushka@Dhanushkas-MacBook-Pro.local>
2026-01-12 22:28:36 +0530

    Remove .next build folder from tracking, add proper .gitignore, add Individual Responses feature with time tracking

--- .next/dev/static/chunks/src_app_[admin]_page_tsx_47b43e25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_eee73213._.js",
-  "static/chunks/src_900d44c5._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_[admin]_surveys_[id]_categories_page_tsx_47b43e25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_0c277fdd._.js",
-  "static/chunks/src_1cfb3d93._.js"
-],
-    source: "dynamic"
-});
